cd283821e9df2f49061822051e4aef295dee73c0
Diego <diegoolliveira1999@gmail.com>
2020-11-11 01:48:20 -0300

    create class repository

---
+++ AcademicSystem_Persitence/src/main/java/br/edu/ifnmg/AcademicSystem_Persitence/DataAccessObject.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.AcademicSystem_Persitence;
+
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.Repository;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author diego
+ *///Esse é uma classe genérica 
+public class DataAccessObject<T> implements Repository<T> {
+    // é aqui que faz as operações de banco de dados 
+    private Class type;
+    private EntityManager manager; //é a repsonsável em fazer tds as configurações de banco de dados
+    
+    public DataAccessObject(Class type){// isso só serve para inicializar o gerenciador de entidade
+        var factory = Persistence.createEntityManagerFactory("UP");//aqui tem que dá o nome que está no arquivo persitece
+        this.manager = factory.createEntityManager();
+        this.type = type;
+    }
+
+    public Class getType() {
+        return type;
+    }
+
+    public EntityManager getManager() {
+        return manager;
+    }
+
+    public void setType(Class type) {
+        this.type = type;
+    }
+
+    public void setManager(EntityManager manager) {
+        this.manager = manager;
+    }
+    
+    @Override
+    public boolean Save(T obg) {
+        EntityTransaction transaction = this.manager.getTransaction();
+        try{
+            transaction.begin();// inicia a transação
+            
+            this.manager.persist(obg);//faz a transação
+            
+            transaction.commit(); // Finalia a transação
+            
+            return true; 
+            
+        }catch(Exception exeption){
+            transaction.rollback();
+        
+            return false;
+        }
+    }
+
+    @Override
+    public boolean Delete(T obg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T Open(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<T> search(T filtro) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ AcademicSystem_Persitence/src/main/java/br/edu/ifnmg/AcademicSystem_Persitence/UserDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.AcademicSystem_Persitence;
+
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.User;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.UserRepository;
+
+/**
+ *
+ * @author diego
+ */
+public class UserDAO extends DataAccessObject<User> implements UserRepository{
+    
+    public UserDAO() {
+        super(User.class);
+    }
+
+    @Override
+    public boolean login(String user, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
