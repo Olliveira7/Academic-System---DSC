f1c7f544c93ff5f3a98cd4b5bb2b209d5e37d8f6
Diego <diegoolliveira1999@gmail.com>
2020-12-07 04:05:07 -0300

    testando sale

--- AcademicSystem_ConsolePresentation/src/main/java/br/edu/ifnmg/AcademicSystem_ConsolePresentation/Console.java
+++ AcademicSystem_ConsolePresentation/src/main/java/br/edu/ifnmg/AcademicSystem_ConsolePresentation/Console.java
@@ -5,7 +5,14 @@
  */
 package br.edu.ifnmg.AcademicSystem_ConsolePresentation;
 
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.Client;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.ClientRepository;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.ItemSale;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.Product;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.ProductRepository;
 import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.RepositoryFactory;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.Sale;
+import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.SaleRepository;
 import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.User;
 import br.edu.ifnmg.AcademicSystem_LogicaAplicacao.UserRepository;
 import java.util.ArrayList;
@@ -42,11 +49,11 @@ public class Console {
 //        Client c = repo.Open(101L);
 //        System.out.println(c.getName() + " , " + c.getCpf());
 //        
-//        ClientRepository repo = new ClientDAO();
+//        ClientRepository repo = RepositoryFactory.getClientRepository();
 //        System.out.println("22222222");
 //        Client c = new Client();
-//        c.setCpf("tttttt");
-//        c.setName("jdeidjiejde");
+//        c.setCpf("123");
+//        c.setName("Maria");
 //        if(repo.Save(c)){
 //            System.out.println("Sucesso");
 //        }else{
@@ -80,21 +87,39 @@ public class Console {
 
        
         
-        UserRepository repo = RepositoryFactory.getUserRepository();
+        SaleRepository repoS = RepositoryFactory.getSaleRepository();
+        UserRepository repoU = RepositoryFactory.getUserRepository();
+        ClientRepository repoC = RepositoryFactory.getClientRepository();
+        ProductRepository repoP = RepositoryFactory.getProductRepository();
         
+        Sale s = new Sale();
         User u = new User();
+        Product p = new Product();
+        Client c = new Client();
+        
+        c = repoC.Open(151L);
+        u = repoU.Open(51L);
+        p = repoP.Open(1L);
+        
+        ItemSale i = new ItemSale(p,5);
+        i.setSale(s);
+        s.setClient(c);
+        s.add(i);
+        s.setUser(u);
+        
+        repoS.Save(s);
 //        u.setCpf("1234");
 //        u.setName("bom");
 //        u.setLogin("dado");
 //        u.setPassword("123");
 //        repo.Save(u);
 //        System.out.println(u.getName());
-       u = repo.login("dado", "123");
-       if(u != null){
-           System.out.println(u.getId());
-       }else{
-           System.out.println("Deu errado");
-       }
+//       u = repo.login("dado", "123");
+//       if(u != null){
+//           System.out.println(u.getId());
+//       }else{
+//           System.out.println("Deu errado");
+//       }
         //Essa parte é para carregar aqueles dados na memória 
         
         
